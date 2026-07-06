00514fcb65888ff69f626af25be7632f0e1ae486
Maria Khomynets <masha.khomynets@gmail.com>
2026-07-06 16:50:08 +0300

    add components and styles

--- 06-notehub-nextjs/app/notes/page.tsx
+++ 06-notehub-nextjs/app/notes/page.tsx
@@ -1,3 +1,17 @@
-export const Notes = () => {
-  return <div>Notes</div>;
+// notes/page.tsx
+
+import { getNotes } from "@/lib/api";
+import NoteList from "@/components/NoteList/NoteList";
+
+const Notes = async () => {
+  const response = await getNotes();
+
+  return (
+    <section>
+      <h1>Notes List</h1>
+      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+    </section>
+  );
 };
+
+export default Notes;
